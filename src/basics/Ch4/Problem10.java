78921c6e07e0763fca794ed6979c2bc6c8b683b2
rejector7 <2643719428@qq.com>
2021-04-14 18:57:24 +0800

    switch exercise10, but not implemented by switch

--- src/basics/Ch4/Problem10.java
+++ src/basics/Ch4/Problem10.java
@@ -1,18 +1,55 @@
 package basics.Ch4;
 import java.util.ArrayList;
+import java.util.HashSet;
+import static java.lang.System.out;
 
 public class Problem10 {
-    private int numLen;
-    private ArrayList<Integer> vampireList;
-    private ArrayList<ArrayList<Integer>> match;
-    public Problem10(int numLen) {
-        this.numLen = numLen;
-//        this.match =
+    private final ArrayList<Integer> vampireList = new ArrayList<>();
+    private final ArrayList<ArrayList<Integer>> match = new ArrayList<>();
+    public Problem10() {
+        for (int i = 1; i < 4; i++) {
+            ArrayList<Integer> tempArrayList = new ArrayList<>();
+            tempArrayList.add(0);
+            tempArrayList.add(i);
+            this.match.add(tempArrayList);
+        }
     }
+
+    private Boolean IsMagic(int target){
+        String targetS = Integer.toString(target);
+        for (ArrayList<Integer> innerArray: this.match){
+            ArrayList<Integer> indexSet = new ArrayList<>();
+            indexSet.add(0);
+            indexSet.add(1);
+            indexSet.add(2);
+            indexSet.add(3);
+            indexSet.remove(innerArray.get(0));
+            indexSet.remove(innerArray.get(1));
+            for (int i=0; i<2; i++){
+                int num1 = innerArray.get(i);
+                int num2 = innerArray.get((i+1)%2);
+                int mul1 = Integer.parseInt(targetS.charAt(num1) + "" + targetS.charAt(num2));
+                for (int j=0; j<2; j++){
+                    int num3 = indexSet.get(j);
+                    int num4 = indexSet.get((j+1)%2);
+                    int mul2 = Integer.parseInt(targetS.charAt(num3) + "" + targetS.charAt(num4));
+                    if(mul2 * mul1 == target){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
     public void run(){
         for (int num = 1000; num < 10000; num++){
-            String numStr = Integer.toString(num);
-
+            if(this.IsMagic(num)){
+                this.vampireList.add(num);
+            }
+        }
+        for (int v: this.vampireList){
+            out.println(v + "");
         }
     }
 }
--- src/basics/Ch4/Test.java
+++ src/basics/Ch4/Test.java
@@ -3,7 +3,7 @@ import basics.Ch4.*;
 
 public class Test {
     public static void main(String[] args){
-        Problem9 p = new Problem9(5);
+        Problem10 p = new Problem10();
         p.run();
 
     }
