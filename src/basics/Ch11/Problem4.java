3d6a8dc30fe86e129afb875d4fe12792d9b53154
rejector7 <2643719428@qq.com>
2021-04-29 13:21:24 +0800

    [add] ch11 Pro4, datagenerator to fill collection.

---
+++ src/basics/Ch11/Problem4.java
@@ -0,0 +1,38 @@
+package basics.Ch11;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+import static util.Print.*;
+
+public class Problem4 {
+    static class DataGenerator{
+        private String[] data;
+        private int length;
+        private int index = 0;
+        public DataGenerator(){
+            data = new String[]{"Snow white", "Star wars"};
+            length = data.length;
+        }
+        public String next(){
+            String res = data[index];
+            index = (index + 1)%2;
+            return res;
+        }
+    }
+    public static Collection<String> fillData(Collection<String> c, int num, Problem4.DataGenerator pd){
+        for(int i= 0;i<num; i++){
+            c.add(pd.next());
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        Problem4.DataGenerator pd = new DataGenerator();
+        println(fillData(new ArrayList<>(), 5, pd));
+        println(fillData(new HashSet<>(), 6, pd));
+    }
+}
+
+
+
+
