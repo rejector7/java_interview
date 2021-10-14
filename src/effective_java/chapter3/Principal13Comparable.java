dcc0781c1deb5114e9c6e4b8d17997e1879b3079
rejector7 <2643719428@qq.com>
2021-10-14 21:28:18 +0800

    [add] Comparable & Comparator

---
+++ src/effective_java/chapter3/Principal13Comparable.java
@@ -0,0 +1,39 @@
+package effective_java.chapter3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Principal13Comparable implements Comparable<Principal13Comparable>{
+    private final int phoneNumber;
+
+    public Principal13Comparable(int phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public int compareTo(Principal13Comparable pc){
+        return Integer.compare(this.phoneNumber, pc.phoneNumber);
+    }
+
+    public String toString(){
+        return String.valueOf(this.phoneNumber);
+    }
+
+    public static void main(String[] args) {
+        Principal13Comparable[] arrayA = new Principal13Comparable[3];
+        arrayA[0] = new Principal13Comparable(123);
+        arrayA[1] = new Principal13Comparable(234);
+        arrayA[2] = new Principal13Comparable(45);
+        Arrays.sort(arrayA);
+        System.out.println(Arrays.toString(arrayA));
+
+        // anonymous class -> lambda expression -> comparingInt
+        Comparator<Principal13Comparable> comparator = Comparator.comparingInt(o -> o.phoneNumber);
+
+        System.out.println(comparator.compare(arrayA[0], arrayA[1]));
+
+
+    }
+
+
+}
